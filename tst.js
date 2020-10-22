99a8348e302bd9da1cb563b93f5973240d89b0bc
AlanMartines <alancpmartines@hotmail.com>
2020-10-22 14:23:26 -0400

    Update

--- tst.js
+++
@@ -1,8 +0,0 @@
-const db = require("./config/db.user");
-(async () => {
-    console.log('Começou!');
- 
-    console.log('SELECT * FROM usuarios');
-    const clientes = await db.selectCustomers();
-    console.log(clientes);
-})();
